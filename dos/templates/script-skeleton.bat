3dab9385706fe512d18a9d265f475e74af8d80ce
Kristian Kutin <kristian.kutin@arcor.de>
2017-12-20 20:38:13 +0100

    Update
    1) Added a cleanup script
    
    2) Updated a script skeleton

--- dos/templates/script-skeleton.bat
+++ dos/templates/script-skeleton.bat
@@ -32,9 +32,10 @@
 @rem ===
 
 call:defineMacros
+call:defineConstants
 
 
-rem ToDo
+rem ToDo - add the main script body
 
 
 %return%
@@ -60,5 +61,108 @@ rem ToDo
 	set "cprint=echo|set /p="
 	
 	set "return=exit /b"
+
+%return%
+
+
+@rem --------------------------------------------------------------------------------
+@rem ---
+@rem ---   void defineConstants()
+@rem ---
+@rem ---   The subroutine defines required constants.
+@rem ---
+
+:defineConstants
+
+	set TRUE=TRUE
+	set FALSE=FALSE
+
+	set LOGFILE=%~n0.log
+
+	rem ToDo - add additional constants
+
+%return%
+
+
+@rem --------------------------------------------------------------------------------
+@rem ---
+@rem ---   void resetErrorlevel()
+@rem ---
+@rem ---   The subroutine resets the error level.
+@rem ---
+
+:resetErrorlevel
+
+%return% 0
+
+
+@rem --------------------------------------------------------------------------------
+@rem ---
+@rem ---   void logInfo(String text)
+@rem ---
+@rem ---   The subroutine logs the specified info text.
+@rem ---
+@rem ---
+@rem ---   @param text
+@rem ---          an info text
+@rem ---
+
+:logInfo
+
+	set "__text=%1"
+	if '%__text%'=='' (
+	
+		call:logError "^(%0^) No text was specified!"
+		%return% 2
+	)
+	set "__text=%__text:"=%"
+
+
+	(
+		%cprintln% %date%::%time%::INFO::%__text%
+	)
+	
+	(
+		%cprintln% %date%::%time%::INFO::%__text%
+	) >> %LOGFILE%
+
+
+	set __text=
+
+%return%
+
+
+@rem --------------------------------------------------------------------------------
+@rem ---
+@rem ---   void logError(String text)
+@rem ---
+@rem ---   The subroutine logs the specified error text.
+@rem ---
+@rem ---
+@rem ---   @param text
+@rem ---          an error text
+@rem ---
+
+:logError
+
+	set "__text=%1"
+	if '%__text%'=='' (
+	
+		call:logError "^(%0^) No text was specified!"
+		%return% 2
+	)
+	set "__text=%__text:"=%"
+
+
+	(
+		echo %date%::%time%::ERROR::%__text%
+	) 1>&2
+
+	(
+		echo %date%::%time%::ERROR::%__text%
+	) >> %LOGFILE%
+
+
+	set __text=
 
 %return%
