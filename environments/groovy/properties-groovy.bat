f73c1dfbec9dd207c55028bd99073112971299ef
Kristian Kutin <kristian.kutin@arcor.de>
2022-02-15 14:20:19 +0100

    new scripts for groovy

---
+++ environments/groovy/properties-groovy.bat
@@ -0,0 +1,8 @@
+@Echo Off
+
+set GROOVY_HOME=D:\tools\groovy-4.0.0\
+set GROOVY_BIN=%GROOVY_HOME%bin\
+set GROOVY_LIB=%GROOVY_HOME%lib\
+set GROOVY_SHELL_BAT=%GROOVY_BIN%groovysh.bat
+set GROOVY_BAT=%GROOVY_BIN%groovy.bat
+set GROOVY_VERSION=4.0.0
---
+++ environments/groovy/properties-java.bat
@@ -0,0 +1,6 @@
+@Echo Off
+
+set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_171\
+set JAVA_BIN=%JAVA_HOME%bin\
+set JAVA_EXE=%JAVA_BIN%java.exe
+set JAVA_VERSION=1.8.0
